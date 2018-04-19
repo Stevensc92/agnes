9cfa98db5842e2a7f0decbfe61587ebc8206a56e
PCServeur\PC-Serveur <stevensc92@gmail.com>
2018-04-19 04:07:55 +0200

    Update path pdo

--- src/Util/DBConnection.php
+++ src/Util/DBConnection.php
@@ -19,10 +19,10 @@ class DBConnection
         {
             $dsn = 'mysql:dbname='.self::$config['db_name'].';host='.self::$config['db_host'];
             $options = array(
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-                PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                // \PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
             );
 
             self::$db = new \PDO($dsn, self::$config['db_user'], self::$config['db_pass'], $options);
