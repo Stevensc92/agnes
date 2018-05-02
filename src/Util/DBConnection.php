a0daede7f9484e2eec3207301b71d011a3215ea3
PCServeur\PC-Serveur <stevensc92@gmail.com>
2018-05-02 02:54:00 +0200

    update options passed to pdo connection

--- src/Util/DBConnection.php
+++ src/Util/DBConnection.php
@@ -19,9 +19,10 @@ class DBConnection
         {
             $dsn = 'mysql:dbname='.self::$config['db_name'].';host='.self::$config['db_host'];
             $options = array(
-                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-                \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::MYSQL_ATTR_INIT_COMMAND   => 'SET NAMES utf8',
+                \PDO::MYSQL_ATTR_FOUND_ROWS     => TRUE,
+                \PDO::ATTR_ERRMODE              => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_PERSISTENT           => true,
                 // \PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
             );
 
