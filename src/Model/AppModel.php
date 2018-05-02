9e455ea7732d231a449a415c8f4008c60f00ec06
PCServeur\PC-Serveur <stevensc92@gmail.com>
2018-05-02 02:57:38 +0200

    add dynamic insert method for called class/model

--- src/Model/AppModel.php
+++ src/Model/AppModel.php
@@ -2,22 +2,14 @@
 namespace Agnes\Model;
 
 use Agnes\Util\DBConnection;
+use Agnes\Util\TableName;
 
 class AppModel
 {
-    // protected $db;
-    //
-    // public function __construct()
-    // {
-    //     $this->db = DBConnection::getInstance();
-    // }
-
     public static function findAll(): array
     {
         $db = DBConnection::getInstance();
-
-        $table = str_replace('Agnes\\Model\\', '', get_called_class());
-        $table = strtolower(str_replace('Model', '', $table));
+        $table = TableName::getTableName(get_called_class());
 
         $stmt = "SELECT * FROM $table";
         $query = $db->query($stmt);
@@ -28,8 +20,78 @@ class AppModel
 
     public static function findById($id)
     {
+        $db = DBConnection::getInstance();
+        $table = TableName::getTableName(get_called_class());
+
+        $stmt = "SELECT * FROM $table WHERE id = :id";
+        $query = $db->prepare($stmt);
+        $query->bindValue(':id', $id, \PDO::PARAM_INT);
+
+        $query->execute();
+        $data = $query->fetchObject(static::class);
+
+        return $data;
+    }
+
+    public function insert($data = '')
+    {
+        $db = DBConnection::getInstance();
+        $table = TableName::getTableName(get_called_class());
+
+        $stmt = "INSERT INTO $table VALUES";
+        $stmt .= "(";
+
+        foreach ($data as $key => $value)
+            $stmt .= ":$key, ";
+
+        $stmt = substr($stmt, 0, -2);
+        $stmt .= ")";
+
+        $query = $db->prepare($stmt);
+
+        foreach($data as $key => $value)
+            $query->bindValue(":$key", $value, (is_int($value) ? \PDO::PARAM_INT : \PDO::PARAM_STR) );
+
+        if ($query->execute() > 0)
+            return $id = $db->lastInsertId();
+
+        return false;
+    }
+
+    public function deleteById($id)
+    {
+        $db = DBConnection::getInstance();
+        $table = TableName::getTableName(get_called_class());
+
+        $stmt = "DELETE FROM $table WHERE id = :id";
+        $query = $db->prepare($stmt);
+        $query->bindValue(':id', $id, \PDO::PARAM_INT);
+
+        if ($query->execute())
+            return true;
+
+        return false;
 
     }
+
+    // public function multiInsert($data)
+    // {
+    //     $db = DBConnection::getInstance();
+    //
+    //     $db->beginTransaction();
+    //
+    //     $table = TableName::getTableName(get_called_class());
+    //
+    //     $stmt = "INSERT INTO $table(`category_id`, `file`) VALUES (:category_id, :file)";
+    //     $db->prepare($stmt);
+    //     $db->bindValue(':category_id', $data['category_id'], \PDO::PARAM_INT);
+    //     $db->bindParam(':file', $file, \PDO::PARAM_STR);
+    //
+    //     foreach ($data['files'] as $file)
+    //     {
+    //         $db->execute();
+    //     }
+    // }
 }
 
 
