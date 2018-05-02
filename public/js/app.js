d719e4c736998dd5e7dc001e872db144a0d1a92f
PCServeur\PC-Serveur <stevensc92@gmail.com>
2018-05-02 02:40:34 +0200

    add js verification for upload form

--- public/js/app.js
+++ public/js/app.js
@@ -9,4 +9,48 @@ jQuery(function($){
 			alert.slideUp(500);
 		})
 	}
+
+    var focus = $('#focus');
+    if (focus.length > 0) {
+        $('html, body').animate({
+            scrollTop: focus.offset().top
+        }, 500);
+    }
+
+    var app = {
+        formSubmit : function() {
+            var form = $('#upload-form');
+            if (form.length > 0)
+            {
+                form.on('submit', function(event){
+                    var valideForm = true;
+                    var errorMessage = '';
+                    if ($('#files')[0].files.length == 0)
+                    {
+                        errorMessage = `${errorMessage} Veuillez sélectionner au moins 1 image à uploader. `;
+                        console.log(errorMessage);
+                        valideForm = false;
+                    }
+
+                    if ($('#category').val() == 'none')
+                    {
+                        if (errorMessage != '')
+                            errorMessage = `${errorMessage} <br>`;
+
+                        errorMessage = ` ${errorMessage} Aucune catégorie sélectionné.`;
+                        valideForm = false;
+                    }
+
+
+                    if (valideForm == false)
+                    {
+                        $('#response').html('<div class="alert alert-error">'+errorMessage+'</div>');
+                        return false;
+                    }
+                });
+            }
+        },
+    };
+
+    app.formSubmit();
 });
