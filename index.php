9702fe507447936c13af99ac62c0b4c4cb26a880
PCServeur\PC-Serveur <stevensc92@gmail.com>
2018-05-02 02:34:23 +0200

    add function is_ajax for ajax method

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ require_once 'vendor/autoload.php';
 
 require 'src/Config/config.php';
 
+function is_ajax()
+{
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
+
 $app = new \Agnes\App;
 $app->run();
 
